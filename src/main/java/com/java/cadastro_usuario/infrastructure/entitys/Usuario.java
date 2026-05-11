e75ec9aa00f062a7226491b885b629d206c45ecd
Leticia Martins De Almeida <lmartinsdealmeida07@gmail.com>
2026-05-11 15:17:58 -0400

    Initial commit

---
+++ src/main/java/com/java/cadastro_usuario/infrastructure/entitys/Usuario.java
@@ -0,0 +1,25 @@
+package com.java.cadastro_usuario.infrastructure.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+
+}
---
+++ src/main/java/com/java/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package com.java.cadastro_usuario.infrastructure.repository;
+
+import com.java.cadastro_usuario.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
+
+
